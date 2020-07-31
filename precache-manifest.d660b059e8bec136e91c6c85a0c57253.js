897842df360253bd2c4765648eeedcfd0a52607d
Brian Li <antai0926@gmail.com>
2020-07-31 17:25:10 +0800

    Updates

---
+++ precache-manifest.d660b059e8bec136e91c6c85a0c57253.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7644d7046396cc04b22f97a30f2a3aed",
+    "url": "/e-commerce-clothing/index.html"
+  },
+  {
+    "revision": "758f3cd84e3ae5726b64",
+    "url": "/e-commerce-clothing/static/css/main.5a0ca9e4.chunk.css"
+  },
+  {
+    "revision": "ffaf73a8cf86c71ebcaf",
+    "url": "/e-commerce-clothing/static/js/2.83b2ffd9.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/e-commerce-clothing/static/js/2.83b2ffd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "758f3cd84e3ae5726b64",
+    "url": "/e-commerce-clothing/static/js/main.2f4f767f.chunk.js"
+  },
+  {
+    "revision": "bf0de64c29f29af57c00",
+    "url": "/e-commerce-clothing/static/js/runtime-main.80c37d9f.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/e-commerce-clothing/static/media/crown.ed28accb.svg"
+  }
+]);
