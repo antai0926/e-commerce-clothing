03c480967edd8e29dca6a4afb6abb6f8dda92eea
Brian Li <antai0926@gmail.com>
2020-08-05 14:25:35 +0800

    dynamic show the cart item count

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import './cart-icon.styles.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 const CartIcon = () => {
   const dispatch = useDispatch();
-
+  const useItemCount = () => {
+    const cartItems = useSelector((state) => state.cart.cartItems);
+    return cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0);
+  };
+  const itemCount = useItemCount();
   return (
     <div className="cart-icon" onClick={() => dispatch(toggleCartHidden())}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 };
