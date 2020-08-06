ae9cd3de17d66c5eb368223c6af1b74634fcc702
Brian Li <antai0926@gmail.com>
2020-08-06 13:46:34 +0800

    add clear item from cart function

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './checkout-item.styles.scss';
+import { useDispatch } from 'react-redux';
+import { clearItemFromCart } from '../../redux/cart/cart.actions';
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => {
+const CheckoutItem = ({ cartItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  const dispatch = useDispatch();
   return (
-    <div className="checkout-item">
+    <div
+      className="checkout-item"
+      onClick={() => dispatch(clearItemFromCart(cartItem))}
+    >
       <div className="image-container">
         <img src={imageUrl} alt="item" />
       </div>
