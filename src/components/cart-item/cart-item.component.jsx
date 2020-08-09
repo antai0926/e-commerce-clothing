9326b491557b93679f7bf76bd244897081cd04e8
Brian Li <antai0926@gmail.com>
2020-08-09 18:48:08 +0800

    use styled component to refactory the cart item, checkout item and collecction priview component

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import './cart-item.styles.scss';
+import {
+  CartItemContainer,
+  CartImage,
+  ItemDetailsContainer,
+} from './cart-item.styles';
 
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
   return (
-    <div className="cart-item">
-      <img src={imageUrl} alt="item" />
-      <div className="item-details">
-        <span className="name">{name}</span>
+    <CartItemContainer>
+      <CartImage src={imageUrl} alt="item" />
+      <ItemDetailsContainer>
+        <span>{name}</span>
         <span className="price">
           {quantity} X ${price}
         </span>
-      </div>
-    </div>
+      </ItemDetailsContainer>
+    </CartItemContainer>
   );
 };
 
--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,38 +1,39 @@
 import React from 'react';
-import './checkout-item.styles.scss';
 import { useDispatch } from 'react-redux';
 import {
   clearItemFromCart,
   addItem,
   removeItem,
 } from '../../redux/cart/cart.actions';
+import {
+  CheckoutItemContainer,
+  ImageContainer,
+  TextContainer,
+  QuantityContainer,
+  RemoveButtonContainer,
+} from './checkout-item.styles';
 
 const CheckoutItem = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
   const dispatch = useDispatch();
   return (
-    <div className="checkout-item">
-      <div className="image-container">
+    <CheckoutItemContainer>
+      <ImageContainer>
         <img src={imageUrl} alt="item" />
-      </div>
-      <span className="name">{name}</span>
-      <span className="quantity">
-        <div className="arrow" onClick={() => dispatch(removeItem(cartItem))}>
-          &#10094;
-        </div>
-        <span className="value">{quantity}</span>
-        <div className="arrow" onClick={() => dispatch(addItem(cartItem))}>
-          &#10095;
-        </div>
-      </span>
-      <span className="price">{price}</span>
-      <div
-        className="remove-button"
+      </ImageContainer>
+      <TextContainer>{name}</TextContainer>
+      <QuantityContainer>
+        <div onClick={() => dispatch(removeItem(cartItem))}>&#10094;</div>
+        <span>{quantity}</span>
+        <div onClick={() => dispatch(addItem(cartItem))}>&#10095;</div>
+      </QuantityContainer>
+      <TextContainer>{price}</TextContainer>
+      <RemoveButtonContainer
         onClick={() => dispatch(clearItemFromCart(cartItem))}
       >
         &#10005;
-      </div>
-    </div>
+      </RemoveButtonContainer>
+    </CheckoutItemContainer>
   );
 };
 
