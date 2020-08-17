79612671bceea86ae89398779e7c1cd88f02db73
Brian Li <antai0926@gmail.com>
2020-08-17 14:27:54 +0800

    use error boundary to show the error page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Header from './components/header/header.component';
 import { checkUserSession } from './redux/user/user.actions';
 import { GlobalStyle } from './global.styles';
 import Spinner from './components/spinner/spinner.component';
+import ErrorBoundary from './components/error-boundary/error-boundary.component.jsx';
 
 const useDetectLogin = () => {
   const dispatch = useDispatch();
@@ -32,18 +33,20 @@ const App = () => {
       <GlobalStyle />
       <Header />
       <Switch>
-        <Suspense fallback={<Spinner />}>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route exact path="/checkout" component={CheckoutPage} />
-          <Route
-            exact
-            path="/signin"
-            render={() =>
-              currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
-            }
-          />
-        </Suspense>
+        <ErrorBoundary>
+          <Suspense fallback={<Spinner />}>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/shop" component={ShopPage} />
+            <Route exact path="/checkout" component={CheckoutPage} />
+            <Route
+              exact
+              path="/signin"
+              render={() =>
+                currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+              }
+            />
+          </Suspense>
+        </ErrorBoundary>
       </Switch>
     </div>
   );
